cdad8452ee22f06ed2c29a25152049878572be2b
m-sepehr <m-sepehr@hotmail.com>
2023-03-02 10:37:42 -0500

    Partial fix for calculation of completion time

--- ch5/project/java/FCFS.java
+++ ch5/project/java/FCFS.java
@@ -18,19 +18,49 @@ public class FCFS implements Algorithm {
         System.out.println("Average Times: Waiting [ " + average_waiting_time + " ]  turnaround [ " + average_turnaround_time + " ]" );
 
     }
-    @Override
-    public void schedule() {
-        while(this.queue.size() !=0 ){
-            System.out.println("Will run " + pickNextTask());
-            pickNextTask().run();
-            System.out.println("completion time: "+pickNextTask().completionTime);
-            total_waiting_time += pickNextTask().getWaitingTime();
-            total_turnaround_time += pickNextTask().getTurnaroundTime();
-            System.out.println("task " + pickNextTask().getName()+ " finished.\n");
-            this.queue.remove(current_task);
 
+    @Override
+    //public void schedule() {
+    //     long startTime = System.currentTimeMillis();
+    //     while(System.currentTimeMillis() - startTime < this.queue.get(0).getBurst()) {
+    //         System.out.println("Will run " + pickNextTask());
+    //         pickNextTask().run();
+    //         System.out.println("completion time: " + (System.currentTimeMillis() - startTime));
+    //         total_waiting_time += pickNextTask().getWaitingTime();
+    //         total_turnaround_time += pickNextTask().getTurnaroundTime();
+    //         System.out.println("task " + pickNextTask().getName() + " finished.\n");
+    //         this.queue.remove(current_task);
+    //         startTime = 0;
+    //     }
+    //    }
+    public void schedule() {
+        for (int i = 0; i < this.queue.size(); i++)
+        {
+            total_turnaround_time += this.queue.get(i).getBurst();
         }
+
+        long startTime = System.currentTimeMillis();
+        long processStartTime = 0;
+        while (System.currentTimeMillis() - startTime < total_turnaround_time)
+        {
+            if (System.currentTimeMillis() - processStartTime < this.queue.get(0).getBurst())
+            {
+                continue;
+            }
+            else
+            {
+                System.out.println("Will run " + pickNextTask());
+                pickNextTask().run();
+                System.out.println("completion time: " + (System.currentTimeMillis() - processStartTime));
+                total_waiting_time += pickNextTask().getWaitingTime();
+                System.out.println("task " + pickNextTask().getName() + " finished.\n");
+                this.queue.remove(current_task);
+                processStartTime = System.currentTimeMillis();
+            }
+        }
+
     }
+
 
     @Override
     public Task pickNextTask() {
